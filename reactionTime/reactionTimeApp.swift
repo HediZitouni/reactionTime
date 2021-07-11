44205ff1933f2fb76acab1604773c1d5d5330e2a
HediZitouni <zitouni.hedi1@gmail.com>
2021-07-11 17:19:21 +0200

    Initial Commit

---
+++ reactionTime/reactionTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  reactionTimeApp.swift
+//  reactionTime
+//
+//  Created by Hedi Zitouni on 11/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct reactionTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
